6c208255f74b64e5ad761ac1c5742002e6a36f80
therter <thorsten.herter@cismet.de>
2013-11-14 13:30:00 +0100

    fixed #2 Remove all gui elements from the CidsLabelStylePanel, which controls unsupported style features

--- src/main/java/de/cismet/jump/sld/editor/CidsLabelStylePanel.java
+++ src/main/java/de/cismet/jump/sld/editor/CidsLabelStylePanel.java
@@ -15,13 +15,9 @@ import com.vividsolutions.jump.workbench.model.Layer;
 import com.vividsolutions.jump.workbench.ui.MultiInputDialog;
 import com.vividsolutions.jump.workbench.ui.style.LabelStylePanel;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
-import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 /**
@@ -42,32 +38,50 @@ public class CidsLabelStylePanel extends LabelStylePanel {
      */
     public CidsLabelStylePanel(final Layer layer, final MultiInputDialog dialog) {
         super(layer, null, dialog, null);
+        this.remove(horizontalAlignmentComboBox);
+        this.remove(horizontalAlignmentLabel);
+        this.remove(horizontalPositionComboBox);
+        this.remove(horizontalPositionLabel);
+        this.remove(verticalAlignmentComboBox);
+        this.remove(verticalAlignmentLabel);
+        this.remove(scaleCheckBox);
+        this.remove(hideOverlappingLabelsCheckBox);
+        this.remove(previewLabel);
         this.remove(previewPanel);
-        previewPanel = new JPanel() {
-
-                @Override
-                protected void paintComponent(final Graphics g) {
-                    super.paintComponent(g); // To change body of generated methods, choose Tools | Templates.
-                }
-            };
-        previewPanel.setBackground(Color.white);
-        previewPanel.setBorder(BorderFactory.createLoweredBevelBorder());
-        previewPanel.setMaximumSize(new Dimension(200, 38));
-        previewPanel.setMinimumSize(new Dimension(200, 38));
-        previewPanel.setPreferredSize(new Dimension(200, 38));
-        this.add(
-            previewPanel,
-            new GridBagConstraints(
-                0,
-                15,
-                2,
-                1,
-                0.0,
-                0.0,
-                GridBagConstraints.WEST,
-                GridBagConstraints.NONE,
-                new Insets(0, 10, 0, 4),
-                0,
-                0));
+        this.remove(outlineButtonPanel);
+        this.remove(showOutlineCheckBox);
+        this.add(new JPanel(), new GridBagConstraints(0, 14, 2, 1, 0.0, 0.0,
+                GridBagConstraints.WEST, GridBagConstraints.NONE,
+                new Insets(190, 0, 0, 0), 0, 0));
+//        previewPanel = new JPanel() {
+//
+//                @Override
+//                protected void paintComponent(final Graphics g) {
+//                    super.paintComponent(g); // To change body of generated methods, choose Tools | Templates.
+//                }
+//            };
+//        previewPanel.setBackground(Color.white);
+//        previewPanel.setBorder(BorderFactory.createLoweredBevelBorder());
+//        previewPanel.setMaximumSize(new Dimension(200, 38));
+//        previewPanel.setMinimumSize(new Dimension(200, 38));
+//        previewPanel.setPreferredSize(new Dimension(200, 38));
+//        this.add(
+//            previewPanel,
+//            new GridBagConstraints(
+//                0,
+//                15,
+//                2,
+//                1,
+//                1.0,
+//                1.0,
+//                GridBagConstraints.WEST,
+//                GridBagConstraints.NONE,
+//                new Insets(10, 10, 45, 4),
+//                0,
+//                0));
+//        this.add(previewLabel,
+//            new GridBagConstraints(0, 14, 2, 1, 0.0, 0.0,
+//                GridBagConstraints.WEST, GridBagConstraints.NONE,
+//                new Insets(15, 0, 0, 0), 0, 0));        
     }
 }
