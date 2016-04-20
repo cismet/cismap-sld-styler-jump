68470035b872515021c81a5b39f32526bc964165
therter <thorsten.herter@cismet.de>
2016-04-20 15:48:10 +0200

    #15 show server side scale hints

--- src/main/java/de/cismet/jump/sld/editor/CidsScalePanel.java
+++ src/main/java/de/cismet/jump/sld/editor/CidsScalePanel.java
@@ -14,6 +14,22 @@ package de.cismet.jump.sld.editor;
 import com.vividsolutions.jump.workbench.model.Layer;
 import com.vividsolutions.jump.workbench.ui.style.ScaleStylePanel;
 
+import org.openide.util.NbBundle;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+import java.awt.Insets;
+
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import de.cismet.cismap.cidslayer.CidsLayer;
+
+import de.cismet.cismap.commons.featureservice.AbstractFeatureService;
 import de.cismet.cismap.commons.gui.MappingComponent;
 
 /**
@@ -27,6 +43,9 @@ public class CidsScalePanel extends ScaleStylePanel {
     //~ Static fields/initializers ---------------------------------------------
 
     public static MappingComponent mappingComponent;
+    /** The same dpi that is used in the MappingComponent to calculate the scale. */
+    private static final int ASSUMED_DPI = 100;
+    private static final double METER_PER_INCH = 0.0254;
 
     //~ Constructors -----------------------------------------------------------
 
@@ -35,10 +54,67 @@ public class CidsScalePanel extends ScaleStylePanel {
      *
      * @param  layer             DOCUMENT ME!
      * @param  mappingComponent  DOCUMENT ME!
+     * @param  service           DOCUMENT ME!
      */
-    public CidsScalePanel(final Layer layer, final MappingComponent mappingComponent) {
+    public CidsScalePanel(final Layer layer,
+            final MappingComponent mappingComponent,
+            final AbstractFeatureService service) {
         super(layer, null);
-        // this.mappingComponent = mappingComponent;
+
+        super.currentScale1Label.setVisible(false);
+        super.currentScaleLabel.setVisible(false);
+        super.currentScaleTextField.setVisible(false);
+        super.spacerPanelBelowCurrentScale.setVisible(false);
+        super.showAtThisScaleButton.setVisible(false);
+
+        if (service instanceof CidsLayer) {
+            final CidsLayer cidsLayer = (CidsLayer)service;
+            final Double maxArea = cidsLayer.getMaxArea();
+
+            if (maxArea != null) {
+                final GridBagConstraints serverScale = new GridBagConstraints();
+                serverScale.gridx = 1;
+                serverScale.gridy = 2;
+                serverScale.insets = new Insets(2, 2, 5, 2);
+                serverScale.anchor = GridBagConstraints.WEST;
+                serverScale.gridwidth = 6;
+
+                final JPanel pan = new JPanel();
+                pan.setLayout(new GridLayout(3, 1));
+                final JCheckBox chBox = new JCheckBox(NbBundle.getMessage(
+                            CidsScalePanel.class,
+                            "CidsScalePanel.CidsScalePanel()"));
+                chBox.setEnabled(false);
+                chBox.setSelected(true);
+
+                final JPanel minPanel = new JPanel(new GridLayout(1, 2));
+                final JLabel minScaleLab = new JLabel(NbBundle.getMessage(
+                            CidsScalePanel.class,
+                            "CidsScalePanel.CidsScalePanel().minScale"));
+                minPanel.add(minScaleLab);
+                final Dimension mapDimension = mappingComponent.getSize(null);
+                final double mapSizeInSqrMeter = mapDimension.getWidth() * mapDimension.getHeight()
+                            * Math.pow(METER_PER_INCH / ASSUMED_DPI, 2);
+                final JTextField minScale = new JTextField("1:"
+                                + (long)(Math.sqrt(maxArea.longValue() / mapSizeInSqrMeter)));
+                minScale.setEnabled(false);
+                minPanel.add(minScale);
+
+                final JPanel maxPanel = new JPanel(new GridLayout(1, 2));
+                final JLabel maxScaleLab = new JLabel(NbBundle.getMessage(
+                            CidsScalePanel.class,
+                            "CidsScalePanel.CidsScalePanel().maxScale"));
+                maxPanel.add(maxScaleLab);
+                final JTextField maxScale = new JTextField("1:1");
+                maxScale.setEnabled(false);
+                maxPanel.add(maxScale);
+
+                pan.add(chBox);
+                pan.add(minPanel);
+                pan.add(maxPanel);
+                super.add(pan, serverScale);
+            }
+        }
     }
 
     //~ Methods ----------------------------------------------------------------
@@ -56,5 +132,10 @@ public class CidsScalePanel extends ScaleStylePanel {
     @Override
     protected double currentScale() {
         return mappingComponent.getScaleDenominator();
+    }
+
+    @Override
+    public String getTitle() {
+        return NbBundle.getMessage(CidsScalePanel.class, "CidsScalePanel.getTitle()");
     }
 }
